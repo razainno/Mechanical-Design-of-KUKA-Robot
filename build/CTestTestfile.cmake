bdec84e47dd72f66aa1f37d761ee0f4fa78de240
Ilyas Raza <m.raza@innopolis.university>
2019-09-27 21:27:28 +0300

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/muhammad/kuuka_ws/src
+# Build directory: /home/muhammad/kuuka_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("box_robot")
---
+++ build/box_robot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/muhammad/kuuka_ws/src/box_robot
+# Build directory: /home/muhammad/kuuka_ws/build/box_robot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
