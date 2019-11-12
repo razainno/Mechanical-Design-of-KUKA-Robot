30e9ea9f99b0fc078ae06088222c8fec5d3b8fcb
Ilyas Raza <m.raza@innopolis.university>
2019-11-12 13:15:28 +0300

    deleted useless filder

--- build/box_robot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/muhammad/kuka_robot_urdf/src/box_robot
-# Build directory: /home/muhammad/kuka_robot_urdf/build/box_robot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/rrbot_control/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/muhammad/kuka_robot_urdf/src/rrbot_control
-# Build directory: /home/muhammad/kuka_robot_urdf/build/rrbot_control
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
